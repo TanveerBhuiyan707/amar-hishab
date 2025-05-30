be72faa8e40fb84fed4a696974dff7191247c073
tanveer <tanvirbullet@gmail.com>
2025-05-30 21:18:56 +0600

    Initial commit

---
+++ partials/header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">📦 StoreApp</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link text-white" href="dashboard.php">🏠 Home</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="add_product.php">➕ Add Product</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="list_product.php">📋 Products</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="add_user.php">👥 Users</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="logout.php">🚪 Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
